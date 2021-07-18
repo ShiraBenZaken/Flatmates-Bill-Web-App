2f11d54280bf76149bb6a4bf0ebb365403523897
SHIRA BEN ZAKEN <67286268+ShiraBenZaken@users.noreply.github.com>
2021-07-18 16:27:21 +0300

    update main and gitignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
-from wtforms import Form
-from flask import Flask , render_template
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
+from flatmates_bill import flat
 
 app = Flask(__name__)
 
@@ -14,18 +15,41 @@ class HomePage(MethodView):
 class BillFormPage(MethodView):
 
     def get(self):
-        return "bye"
+        bill_form = BillForm()
+        return render_template('bill_form_page.html', billform=bill_form)
+
 
 class ResultsPage(MethodView):
-    pass
 
+    def post(self):
+        billform = BillForm(request.form)
 
-class FormPage(Form):
-    pass
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+        return render_template('results.html',
+                               name1=flatmate1.name,
+                               amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name,
+                               amount2=flatmate2.pays(the_bill, flatmate1))
+
+class BillForm(Form):
+    amount = StringField("Bill Amount: ", default="100")
+    period = StringField("Bill Period: ", default="December 2021")
+
+    name1 = StringField("Name: ", default="Dan")
+    days_in_house1 = StringField("Days in the house: ", default="20")
+
+    name2 = StringField("Name: ", default="Gad")
+    days_in_house2 = StringField("Days in the house: ", default="15")
+
+    button = SubmitField("Calculate")
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results', view_func=ResultsPage.as_view('results'))
 
 app.run(debug=True)
 
